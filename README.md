431a28a86df1bd2d5f2f648c4781a657d625c3bc
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-07-28 14:32:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# github-card-project
+ Página Web que consome a API do github para personalização de um card.
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/users/:login', async (req, res) => {
+    try {
+        const { login } = req.params
+        const { data } = await axios('https://api.github.com/users/' + login)
+        console.log(data)
+        return res.json(data)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.listen('3000')
