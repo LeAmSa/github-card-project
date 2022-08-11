095e42c562624d802d0b1060f4896d7b0f6952ba
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-08-11 09:50:30 -0300

    Create scrollreveal.js

---
+++ src/scripts/scrollreveal.js
@@ -0,0 +1,18 @@
+ScrollReveal({ 
+    // reset: true,
+    distance: '60px',
+    duration: 1500,
+    delay: 400 
+});
+
+ScrollReveal().reveal('.img-examples-ct .img-example', { 
+    delay: 500,
+    origin: 'bottom',
+    interval: 200 
+});
+
+ScrollReveal().reveal('.intro-text h1, .intro-text h2, .intro-text button', { 
+    delay: 500,
+    origin: 'right',
+    interval: 200
+});
